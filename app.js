0f35e6217ffd05617f3b7200f57af8ea58fd5e05
Alex Bozzetto <epedano@gmail.com>
2021-11-10 20:17:11 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+app.get('/registro', (req,res)=>{
+    res.sendFile(__dirname + '/views/registro.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.post("/registro",(req,res)=>{
+    res.redirect("/")
+    
+})
