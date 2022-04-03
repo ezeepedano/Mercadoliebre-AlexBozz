e62286772f6388e0c1b035ab98f5c22d89cd1efd
AlexBozzetto <epedano@gmail.com>
2022-04-03 00:35:48 -0300

    Version Final
    Nunca actualice la version terminada

---
+++ README.md
@@ -0,0 +1,21 @@
+# Mercado Liebre
+Clon de Mercado Libre desarrollado utilizando Express para el back-end y EJS como motor de vistas para el front-end.
+
+## Features
+- Visualizar, crear, editar y eliminar productos (CRUD).
+- Registro e inicio de sesión utilizando session y cookies.
+- Permisos especiales según el tipo de usuario para realizar determinadas tareas.
+
+## Dependencias utilizadas
+- Express
+- EJS
+- Express Session
+- Cookie Parser
+- Method Override
+- Multer
+- Bcrypt
+
+# Usuarios y permisos
+- Administrador (admin / admin): Puede crear, editar y borrar productos.
+- Usuario (user / user): Puede crear productos.
+- Invitado (no logueado): Únicamente puede visualizar productos.
--- app.js
+++ app.js
@@ -1,24 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
+const methodOverride = require("method-override");
+const session = require("express-session");
+const mainRoutes = require("./routes/mainRoutes");
+const productRoutes = require("./routes/productRoutes");
+const authRoutes = require("./routes/authRoutes");
+const authenticateMiddleware = require("./middlewares/authenticateMiddleware");
+
+//Middlewares
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+app.use(session({ secret: "Mensaje secreto" }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(authenticateMiddleware);
 
 
+//Motor de vistas
+app.set("view engine", "ejs");
+app.set("views", [
+    __dirname + "/views",
+    __dirname + "/views/users",
+    __dirname + "/views/products",
+    __dirname + "/views/partials",
+]);
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log('Servidor funcionando');
+// Levantando el servidor
+app.listen(3000, () => {
+    console.log("Server running on port 3000.");
 });
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
-app.get('/registro', (req,res)=>{
-    res.sendFile(__dirname + '/views/registro.html');
-});
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+//Rutas
+app.use("/", mainRoutes);
+app.use("/auth", authRoutes);
+app.use("/products", productRoutes);
+
+// Setea 'user' dentro de locals para usarla despues
+app.locals.user = null;
+
+//404
+app.get("*", (req, res) => {
+    res.send("Error 404 - No encontramos esta página :/");
 });
 
-app.post("/registro",(req,res)=>{
-    res.redirect("/")
-    
-})
